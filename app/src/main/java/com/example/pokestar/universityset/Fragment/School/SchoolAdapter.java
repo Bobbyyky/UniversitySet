66d39959bb633f7f1a51482f337e240d8fa3cf1e
Bobbyyky <281705876@qq.com>
2018-06-19 17:18:52 +0800

    First commmit

---
+++ app/src/main/java/com/example/pokestar/universityset/Fragment/School/SchoolAdapter.java
@@ -0,0 +1,59 @@
+package com.example.pokestar.universityset.Fragment.School;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.pokestar.universityset.R;
+
+import java.util.List;
+
+/**
+ * Created by Shinelon on 2018/6/7.
+ */
+
+public class SchoolAdapter extends RecyclerView.Adapter<SchoolAdapter.ViewHolder>{
+
+    private List<School> mSchoolList;
+
+    public SchoolAdapter(List<School> schoolList){
+        mSchoolList = schoolList;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView schoolImage;
+        TextView schoolTitle;
+        TextView schoolContent;
+
+        public ViewHolder(View view) {
+            super(view);
+            schoolImage = (ImageView)view.findViewById(R.id.school_image);
+            schoolTitle = (TextView)view.findViewById(R.id.school_title);
+            schoolContent = (TextView)view.findViewById(R.id.school_content);
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.school_item,parent,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        School school = mSchoolList.get(position);
+        holder.schoolImage.setImageResource(school.getImageId());
+        holder.schoolTitle.setText(school.getTitle());
+        holder.schoolContent.setText(school.getContent());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSchoolList.size();
+    }
+
+}
---
+++ app/src/main/java/com/example/pokestar/universityset/Fragment/SchoolFragment.java
@@ -0,0 +1,69 @@
+package com.example.pokestar.universityset.Fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pokestar.universityset.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link SchoolFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link SchoolFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SchoolFragment extends Fragment {
+
+    private String TAG = MainActivity.class.getSimpleName();
+
+
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SchoolFragment.
+     */
+
+    public static SchoolFragment newInstance() {
+        SchoolFragment fragment = new SchoolFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG,"高校onCreate");
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_school, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d(TAG,"高校onAttact");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+
+}
